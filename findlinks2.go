ab6dc4e88aa0671aa4bfdd1d0c1405cfb93833ac
yu <peacetomori1118@gmail.com>
2021-04-08 21:31:45 +0900

    concurrent programming day3

---
+++ findlinks2.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"fmt"
+	"gopl.io/ch5/links"
+	"log"
+	"os"
+)
+
+var tokens = make(chan struct{}, 20)
+
+func crawl(url string) []string {
+	fmt.Println(url)
+	tokens <- struct{}{}
+	list, err := links.Extract(url)
+	<-tokens
+
+	if err != nil {
+		log.Print(err)
+	}
+	return list
+}
+
+func main() {
+	worklist := make(chan []string)
+	var n int
+
+	// Start with the command-line arguments.
+	n++
+	go func() { worklist <- os.Args[1:] }()
+
+	// Crawl the web concurrently.
+	seen := make(map[string]bool)
+	for ; n > 0; n-- {
+		list := <-worklist
+		for _, link := range list {
+			if !seen[link] {
+				seen[link] = true
+				n++
+				go func(link string) {
+					worklist <- crawl(link)
+				}(link)
+			}
+		}
+	}
+
+}
